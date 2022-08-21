4316e0feab67b19e8de81d2f0fc75164247cf4ec
chenhong231 <chenhong231@outlook.com>
2022-08-21 17:16:59 +0800

    add chapter7 spring ioc dependency sources

---
+++ dependency-source/src/main/java/org/thinking/spring/dependency/source/ResolvableDependencySourceDemo.java
@@ -0,0 +1,34 @@
+package org.thinking.spring.dependency.source;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.annotation.PostConstruct;
+
+public class ResolvableDependencySourceDemo {
+
+    @Autowired
+    private String value;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        applicationContext.register(ResolvableDependencySourceDemo.class);
+
+        applicationContext.addBeanFactoryPostProcessor(beanFactory -> {
+            beanFactory.registerResolvableDependency(String.class, "Hello,World");
+        });
+
+        applicationContext.refresh();
+
+        applicationContext.close();
+    }
+}
